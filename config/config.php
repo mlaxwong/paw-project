2a92fe80e43f19f9aecae2d8e91b1b19f0bb53eb
Mlax Wong <mlaxwong@gmail.com>
2019-02-14 17:45:20 +0800

    working on cp

--- config/config.php
+++ config/config.php
@@ -2,6 +2,7 @@
 return [
     'basePath' => PATH_BASE,
     'vendorPath' => PATH_VENDOR,
+    'layoutPath' => '@app/views/_layouts',
     'modules' => [
         'admin' => paw\cp\Module::class
     ],
@@ -35,5 +36,19 @@ return [
                 ],
             ],
         ],
-    ]
+        'urlManager' => [
+            'rules' => [
+                '<handle:^((?!admin)(?!test))[\w-\/]+>' => 'site/index',
+            ],
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => env('DB_DSN', 'mysql:host=localhost;port=3306;dbname=database_v1'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'tablePrefix' => env('DB_TABLE_PREFIX', 'prefix_'),
+            'charset' => 'utf8',
+            'enableSchemaCache' => YII_ENV_PROD,
+        ],
+    ],
 ];
