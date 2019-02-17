6349c6884e5e8c2f274b0459fdba589fada3c057
Mlax Wong <mlaxwong@gmail.com>
2019-02-18 00:14:17 +0800

    update paw-cp

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -7,10 +7,18 @@ class SiteController extends Controller
 {
     public function actionIndex($handle = null)
     {
-        echo \paw\cp\collections\Product::find()->one()->id;
-        $model = new \paw\cp\collections\Product;
-        $model->save();
-        print_r($model->errors);
+        // echo \paw\cp\collections\Product::find()->one()->id;
+        // $model = new \paw\cp\collections\Product;
+        // $model->save();
+        // print_r($model->errors);
+
+        // $resources = \paw\cp\resources\Product::getInstance();
+        // $products = $resources->limit(3)->all();
+        // echo '<pre>';
+        // print_r($products);
+        // print_r($resources->dataProvider->pagination->page);
+        // die;    
+
         return $this->render('index');
     }
 }
